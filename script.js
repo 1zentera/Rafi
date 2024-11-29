a5379f3865ca10a5edbeb8316f41da56717d05c4
hetpatel7567 <hetp2758@gmail.com>
2024-11-29 22:46:21 +0530

    final version

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date('Dec 1, 2023 00:00:00').getTime(),
+let countDown = new Date('Dec 1, 2024 00:00:00').getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
